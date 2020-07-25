40a3cd4ced9e3f916adea8280d0c9aff22a35d5f
kalandershihab <kalendershihab@gmail.com>
2020-07-26 00:38:44 +0530

    frontend updated

---
+++ frontend-new/frontendnew/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { TwitterComponent } from './twitter/twitter.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    WhatsappComponent,
+    InstagramComponent,
+    TwitterComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
